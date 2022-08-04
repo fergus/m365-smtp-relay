dfe70bc777cec06c151f4aa4b2c52bea91198ee4
LBegnaud <lbegnaud@radersolutions.com>
2022-08-04 11:38:01 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -27,7 +27,7 @@ if [ ! -z "${EMAIL}" ] && [ ! -z "${EMAILPASS}" ]; then
         echo '' > /etc/postfix/smtp_header_checks
         for addr in $exclusions
         do
-                echo "/From: <$addr>/ PASS" >> /etc/postfix/smtp_header_checks
+                echo "/.*$addr.*/ PASS no masquerade of this from address" >> /etc/postfix/smtp_header_checks
         done
         echo '/From:(.*)/ REPLACE Reply-To:${1}' >> /etc/postfix/smtp_header_checks
         echo "/To:(.*)/ PREPEND From: $EMAIL" >> /etc/postfix/smtp_header_checks
