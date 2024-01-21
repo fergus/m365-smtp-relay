e655549b9d023809c2c612d67b2b021d13b41ae7
Fergus Stevens <fstevens@intervl.com>
2024-01-21 10:43:15 +0000

    tweak for new connector

--- init.sh
+++ init.sh
@@ -2,14 +2,14 @@
 
 # Set timezone
 if [ ! -z "${SYSTEM_TIMEZONE}" ]; then
-    echo "configuring system timezone"
+    echo "Configuring system timezone"
     echo "${SYSTEM_TIMEZONE}" > /etc/timezone
     dpkg-reconfigure -f noninteractive tzdata
 fi
 
 # Set mynetworks for postfix relay
 if [ ! -z "${MYNETWORKS}" ]; then
-    echo "setting mynetworks = ${MYNETWORKS}"
+    echo "Setting mynetworks = ${MYNETWORKS}"
     postconf -e mynetworks="${MYNETWORKS}"
 fi
 
@@ -21,8 +21,8 @@ fi
 
 # General the email/password hash and remove evidence.
 if [ ! -z "${EMAIL}" ] && [ ! -z "${EMAILPASS}" ]; then
-#    echo "[smtp.gmail.com]:587    ${EMAIL}:${EMAILPASS}" > /etc/postfix/sasl_passwd
-    echo "[SMTP.office365.com]:587    ${EMAIL}:${EMAILPASS}" > /etc/postfix/sasl_passwd
+    #echo "[SMTP.office365.com]:587    ${EMAIL}:${EMAILPASS}" > /etc/postfix/sasl_passwd
+    echo "[intervl-com.mail.protection.outlook.com]:25    ${EMAIL}:${EMAILPASS}" > /etc/postfix/sasl_passwd
     postmap /etc/postfix/sasl_passwd
     #rm /etc/postfix/sasl_passwd
     ## remove FROM header, set reply-to, and insert FROM to be auth username
